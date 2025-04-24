ac901873a2479cb237f11a741e2cc6e3862593e3
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2025-04-24 02:00:11 -0600

    Update main.py
    Main updated to a main class and the functionality added to open the file

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from scanner.scanner_main import scanner as sc
+from scanner import tokens
 
 # Here we are going to design the command line interface that we will be
 # using to interact with the user
 
-welcome = ''' 
+front_page = ''' 
 Instituto Tecnológico de Costa Rica
 Ingeniería en Computación
 Compiladores e intérpretes
@@ -17,26 +19,34 @@ Presiona:
     4. Para aceptar el token TomeToke()
     '''
 
-print(welcome)
+def main():
 
-while(True):
-    response = input()
+    print(front_page) # Here we show the banner with the explanation on how to interact
 
-    match response:
-        case "1":
-            print("Iniciando el scanner...")
-            # Aquí se inicializa el scanner
-        case "2":
-            print("Finalizando el scanning...")
-            # Aquí se finaliza el scanning
-        case "3":
-            print("El token siguiente es:")
-            # It reads the token and it identifies what familiy belongs to
-            # then it shows to the user the family code, token, translated
-            # value, line and column, and error line if necessary
-        case "4":
-            print("Aceptamos el token")
-            # Here we decide if the token is going to be accepted or not
-        case _:
-            print("Opción no válida. Por favor, ingrese una opción válida.")
-            # Aquí se maneja la opción no válida
+    while(True):
+        response = input()
+
+        match response:
+            case "1":
+                print("Ingrese el directorio del archivo")
+                path = input()                              # Request the path from the user
+                newScanner = sc(path)                       # We instance the Scanner as a new object
+                newScanner.InicializarScanner()             # In this function we check if the path is valid and it loads it
+
+            case "2":
+                print("Finalizando el scanning...")
+                # Aquí se finaliza el scanning
+            case "3":
+                print("El token siguiente es:")
+                # It reads the token and it identifies what familiy belongs to
+                # then it shows to the user the family code, token, translated
+                # value, line and column, and error line if necessary
+            case "4":
+                print("Aceptamos el token")
+                # Here we decide if the token is going to be accepted or not
+            case _:
+                print("Opción no válida. Por favor, ingrese una opción válida.")
+                # Aquí se maneja la opción no válida
+
+if __name__ == "__main__":
+    main()
